e6469d21d3810c176ebdb19b4f8d7f77a2b811b8
YoonWonYoul <yoonwonyoul@webchemist.net>
2021-11-04 22:29:19 +0900

    for, while, nullable

--- app/src/main/java/com/example/kotlin/Chapter 06. 반복문.kt
+++ app/src/main/java/com/example/kotlin/Chapter 06. 반복문.kt
@@ -1,3 +1,31 @@
 package com.example.kotlin
 
-fun main() {}
+fun main() {
+    // For
+    val students = arrayListOf("gen", "lee", "kim", "park")
+
+    for (name in students) {
+        println(name)
+    }
+
+    var sum: Int = 0;
+    for ( i in 1..10) {
+        sum += i
+    }
+
+    println(sum)
+
+    for ( i in 1..10 step 2) { }
+    for ( i in 10 downTo 1) { }
+    for (i in 1 until 100) { }
+    for ((index, name) in students.withIndex()) {
+        println("${index + 1}번째 학생 : ${name}")
+    }
+
+    // While
+    var index = 0;
+    while(index < 10) {
+        println("current index: ${index}")
+        index++
+    }
+}
--- app/src/main/java/com/example/kotlin/Chapter 07. nonnull과 nullable.kt
+++ app/src/main/java/com/example/kotlin/Chapter 07. nonnull과 nullable.kt
@@ -1,3 +1,26 @@
 package com.example.kotlin
 
-fun main() {}
+fun main() {
+    // NPE : Null Pointer Exception
+
+    var name : String = "genn"
+    var nullName: String? = null
+    var nameInUpperCase = name.uppercase()
+    var nullNameInUpperCase = nullName?.uppercase()
+
+    val lastName: String? = null
+    val fullName: String = name + (lastName ?: "No lastName") // null 이라면 대체합니다.
+
+    println(fullName)
+}
+
+fun ignoreNulls(str: String?) {
+    val mNotNull: String = str!! // 절대로 null 일 수 없다고 지정
+    val upper: String = mNotNull.uppercase()
+
+    // null 아닐때만 동작
+    val email: String? = "genn@company.com"
+    email?.let {
+        println("my email is ${email}")
+    }
+}
