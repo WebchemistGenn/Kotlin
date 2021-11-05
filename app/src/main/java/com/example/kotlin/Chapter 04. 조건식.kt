86e8dfd1ddad75fc97c5433606eca7ce0e958ae5
YoonWonYoul <yoonwonyoul@webchemist.net>
2021-11-05 14:58:06 +0900

    add class

--- app/src/main/java/com/example/kotlin/Chapter 04. 조건식.kt
+++ app/src/main/java/com/example/kotlin/Chapter 04. 조건식.kt
@@ -1,3 +1,11 @@
 package com.example.kotlin
 
-fun main() {}
+fun main() {
+    val x = 20
+    val y = 10
+    if (x < y) {
+        println("Y가 더 큽니다.")
+    } else {
+        println("X가 더 큽니다.")
+    }
+}
--- app/src/main/java/com/example/kotlin/Chapter 08. class.kt
+++ app/src/main/java/com/example/kotlin/Chapter 08. class.kt
@@ -1,3 +1,32 @@
 package com.example.kotlin
 
-fun main() {}
+open class Person(val name: String = "default name") {
+    init {
+        println("New Person!")
+    }
+    fun eat() {
+        println("YUMM!")
+    }
+
+    open fun sleep() {
+        println("Sleep")
+    }
+}
+
+class Test: Person {
+    constructor(name: String = "test", age: Int) {
+        println("new Test")
+    }
+
+    override fun sleep() {
+        super.sleep()
+        println("test sleep")
+    }
+}
+
+fun main() {
+    val genn = Person("Genn")
+    genn.eat()
+
+    println("my name is ${genn.name}")
+}
